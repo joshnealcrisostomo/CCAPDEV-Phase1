543e5fa3bdffb44dd990c99a698e6f8051d85bfc
joshnealcrisostomo <158022555+joshnealcrisostomo@users.noreply.github.com>
2025-02-03 19:36:17 +0800

    50%

--- javascript/viewComments.js
+++ javascript/viewComments.js
@@ -7,12 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
         // Populating the profile page
         document.getElementById("displayName").textContent = comments[username].displayName;
         document.getElementById("comment-username").textContent = comments[username].postusername;
-        document.getElementById("postduration").textContent = comments[username].postduration;
-        document.getElementById("postTitle").textContent = comments[username].postTitle;
-        document.getElementById("posterpfp").src = comments[username].posterpfp;
-        document.getElementById("postContent").textContent = comments[username].postContent;
-        document.getElementById("postImage").src = comments[username].postImage;
-        document.getElementById("votes").textContent = comments[username].votes;
+        document.getElementById("comment-duration").textContent = comments[username].postduration;
+        document.getElementById("comment-title").textContent = comments[username].postTitle;
+        document.getElementById("comment-content").textContent = comments[username].postContent;
+        document.getElementById("comment-votes").textContent = comments[username].votes;
     } else {
         document.querySelector(".middle-panel").innerHTML = "<h2>User Not Found</h2>";
     }

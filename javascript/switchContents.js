be83d0804fe189dc989f700fca95a24919033fec
joshnealcrisostomo <158022555+joshnealcrisostomo@users.noreply.github.com>
2025-02-03 10:12:18 +0800

    sample 3 done

--- javascript/switchContents.js
+++ javascript/switchContents.js
@@ -121,6 +121,18 @@ function redirectToSamplePost2() {
     window.location.href = 'sample2.html'
 }
 
+function redirectToSamplePost3() {
+    window.location.href = 'sample3.html'
+}
+
+function redirectToSamplePost4() {
+    window.location.href = 'sample4.html'
+}
+
+function redirectToSamplePost5() {
+    window.location.href = 'sample5.html'
+}
+
 function redirectToSettings() {
     window.location.href = 'settings.html';
 }

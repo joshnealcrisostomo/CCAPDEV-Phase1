d69ef62db3fbef8df53bb4a8f4ebe05472fc2591
joshnealcrisostomo <158022555+joshnealcrisostomo@users.noreply.github.com>
2025-02-05 11:12:31 +0800

    fixing edit/delete

---
+++ javascript/delete.js
@@ -0,0 +1,15 @@
+function deletePost() {
+    const confirmation = confirm("Are you sure you want to delete this post?");
+    if (confirmation) {
+        // Add code here to delete the post, e.g., remove it from the DOM
+        alert("Post deleted successfully!"); // Temporary feedback, replace with actual delete logic
+    }
+}
+
+function deleteComment() {
+    const confirmation = confirm("Are you sure you want to delete this comment?");
+    if (confirmation) {
+        // Add code here to delete the post, e.g., remove it from the DOM
+        alert("Post deleted successfully!"); // Temporary feedback, replace with actual delete logic
+    }
+}
--- javascript/switchContents.js
+++ javascript/switchContents.js
@@ -67,3 +67,8 @@ function redirectToEditProfile() {
 function redirectToPost(username) {
     window.location.href = `sample3.html?user=${username}`;
 }
+
+function redirectToEditPost() {
+    window.location.href = `editPost.html`;
+}
+

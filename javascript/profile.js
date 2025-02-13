e2c98a0cf2d7a518986731f571a4aac11cfbab92
joshnealcrisostomo <158022555+joshnealcrisostomo@users.noreply.github.com>
2025-02-13 15:25:07 +0800

    handlebars for profile

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const params = new URLSearchParams(window.location.search);
     const username = params.get("user");
+    const profileContainer = document.getElementById("profile-container");
 
     if (users[username]) {
-        // Populating the profile page
-        document.getElementById("profile-name").textContent = users[username].displayName;
-        document.getElementById("display-name").textContent = users[username].displayName;  // for header display
-        document.getElementById("username").textContent = users[username].username;
-        document.getElementById("bio").textContent = users[username].bio;
-        document.getElementById("profile-pic").src = users[username].profilePic;
+        const source = document.getElementById("profile-template").innerHTML;
+        const template = Handlebars.compile(source);
+        const profileHTML = template(users[username]);
+        profileContainer.innerHTML = profileHTML;
     } else {
-        document.querySelector(".middle-panel").innerHTML = "<h2>User Not Found</h2>";
+        profileContainer.innerHTML = "<h2>User Not Found</h2>";
     }
 });
